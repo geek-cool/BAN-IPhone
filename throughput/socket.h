e81debc14e80cba8739c2d6dd83f342722e5dc30
東羣 張 <higashibari@dongqundeMacBook-Air.local>
2014-07-21 15:44:58 +0800

    test

--- throughput/socket.h
+++ throughput/socket.h
@@ -9,16 +9,30 @@
 #import <UIKit/UIKit.h>
 #import <sys/socket.h>
 
-@interface ViewController : UIViewController <NSStreamDelegate>
+//@protocol sockDelegate <NSObject>
+//@optional
+//
+//@end
+
+@interface socketDelegate : NSObject  <NSStreamDelegate>
 {
     NSInputStream *inputStream;
     NSOutputStream *outputStream;
+    Boolean *sendkey;
 }
-@property (weak, nonatomic) IBOutlet UITextField *inputNameField;
-@property (weak, nonatomic) IBOutlet UIView *joinView;
+//@property (weak, nonatomic) IBOutlet UITextField *inputNameField;
+//@property (weak, nonatomic) IBOutlet UIView *joinView;
 @property (nonatomic, retain) NSInputStream *inputStream;
 @property (nonatomic, retain) NSOutputStream *outputStream;
+@property Boolean *sendkey;
+//@property (nonatomic,assign) id <sockDelegate> sockdelegate;
 
 //- (void)stream:(NSString *)writeStream handleEvent:(NSStreamEvent)eventCode;
+
+- (void)initNetworkCommunication;
+- (void)closeNetwork;
+- (void)stream:(NSStream *)writeStream handleEvent:(NSStreamEvent)eventCode;
+
+- (void)writeToServer:(float)accel_x accel_y:(float)accel_y accel_z:(float)accel_z gyro_x:(float)gyro_x gyro_y:(float)gyro_y gyro_z:(float)gyro_z;
 
 @end
