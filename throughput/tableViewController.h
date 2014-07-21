44b5fef5fe1e768869430c26738a4b0c3a33e8e5
東羣 張 <higashibari@dongqundeMacBook-Air.local>
2014-07-21 15:45:57 +0800

    test

--- throughput/tableViewController.h
+++ throughput/tableViewController.h
@@ -7,7 +7,49 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "TIBLECBKeyfob.h"
+#import "socket.h"
+//#import "ViewController.h"
 
-@interface tableViewController : UITableViewController
+@interface tableViewController : UITableViewController<TIBLECBKeyfobDelegate, UITableViewDataSource>{
+    TIBLECBKeyfob *t;
+    socketDelegate *socket;
+    NSArray *arryInstructions;
+    NSMutableArray *arryDevices;
+    NSMutableArray *arryConnect;
+    NSString *fileName;
+    NSString *paths;
+    NSString *infile;
+    NSString *peripherals;
+    
+    NSTimer *myTimer;
+    
+    IBOutlet UIProgressView *TIBLEUIAccelXBar;
+    IBOutlet UIProgressView *TIBLEUIAccelYBar;
+    IBOutlet UIProgressView *TIBLEUIAccelZBar;
+    IBOutlet UIButton *ConnectPeri;
+    IBOutlet UITextField *Act;
+    
+    float                       accX;
+    float                       accY;
+    float                       accZ;
+    float                       GYROX;
+    float                       GYROY;
+    float                       GYROZ;
+    //NSMutableArray *arryDone;
+}
+@property (nonatomic, retain) IBOutlet UIActivityIndicatorView *spinner;
+@property (retain, nonatomic) TIBLECBKeyfob *t;
+@property (retain, nonatomic) socketDelegate *socket;
+
+@property (nonatomic, retain) IBOutlet UIProgressView *TIBLEUIAccelXBar;
+@property (nonatomic, retain) IBOutlet UIProgressView *TIBLEUIAccelYBar;
+@property (nonatomic, retain) IBOutlet UIProgressView *TIBLEUIAccelZBar;
+@property (nonatomic, retain) IBOutlet UIButton *ConnectPeri;
+
+@property (retain, nonatomic) NSString *fileName;
+@property (retain, nonatomic) NSString *paths;
+@property (retain, nonatomic) NSString *infile;
+@property (retain, nonatomic) NSString *peripherals;
 
 @end
